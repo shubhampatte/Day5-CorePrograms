2a7f3b2eaea61c492e474ae97f70af6ce83ad2f3
shubhampatte <shubhampatte@gmail.com>
2021-10-03 20:25:23 +0530

    [shubham]:Quotient and Remainder program Added

---
+++ queAndrem.java
@@ -0,0 +1,17 @@
+public class queAndrem
+{
+	public static void main(String args[])
+	{
+		int dividend = 25;
+		int divisior = 2;
+		
+		System.out.println("Dividend is : " +dividend+ " and Divisior is : " +divisior);
+			
+		float quotient = (float) dividend / divisior;
+		int remainder = dividend % divisior;
+			
+		System.out.println("Quotient is : " +quotient+ " and Remainder is : " +remainder);	
+		
+	}	
+
+}
